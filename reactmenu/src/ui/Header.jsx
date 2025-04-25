2181ec9d1587d4b50c24b443f75acf0d567eee67
John Fernando Bernad <johnbernad6@gmail.com>
2025-04-25 10:57:58 -0500

    Update Header.jsx

--- reactmenu/src/ui/Header.jsx
+++ reactmenu/src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import { LuPizza } from "react-icons/lu";
-import { FaGithub } from "react-icons/fa";
+import { LuScrollText } from "react-icons/lu"
 
 import { Link } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
@@ -21,12 +21,13 @@ const Header = () => {
       </div>
       <div className="flex w-16 items-center justify-end gap-2 sm:w-1/4">
         <CartOverview />
-        <a
-          href="https://github.com/iamhosen/"
-          className="relative rounded-full border p-2 text-gray-900 transition-all duration-500 ease-in-out hover:bg-gray-800 hover:text-white"
-        >
-          <FaGithub className="text-2xl" />
-        </a>
+     <Link to="/order" className="flex w-16 items-center gap-2 sm:w-1/4">
+        <LuScrollText className="h-8 w-8 text-orange-600" />
+        <div className="hidden sm:block">
+          <h1 className="-mb-1 text-2xl font-bold">React-Restaurant</h1>
+          <Username />
+        </div>
+      </Link>
       </div>
     </header>
   );
