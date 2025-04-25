da4d0526299bebaae1ec2e157dd5b4e650bea541
John Fernando Bernad <johnbernad6@gmail.com>
2025-04-25 15:08:12 -0500

    Update CreateOrder.jsx

--- reactmenu/src/features/order/CreateOrder.jsx
+++ reactmenu/src/features/order/CreateOrder.jsx
@@ -7,7 +7,7 @@ import store from "../../store";
 import { clearCart, getTotalCartPrice } from "../cart/cartSlice";
 import { formatCurrency } from "../../utils/helpers";
 import { fetchAddress } from "../user/userSlice";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
